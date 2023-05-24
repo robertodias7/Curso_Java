1a0e27fc9a5f3bcb090c35f71b05559c8c54b8d8
robertodias7 <dias.roberto.16@gmail.com>
2023-05-24 15:25:45 -0300

    Novos exercícios

---
+++ DoWhile/src/app/Program.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+       Locale.setDefault(Locale.US);
+       Scanner sc = new Scanner(System.in);
+
+       char resp;
+       do {
+           System.out.print("Digite a temperatura em celsius: ");
+           double C = sc.nextDouble();
+           double F = 9.0 * C/ 5.0 + 32.0;
+           System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
+           System.out.print("Deseja continuar? (s/n): ");
+           resp =  sc.next().charAt(0);
+
+       }while (resp != 0);
+
+       sc.close();
+    }
+}
---
+++ POO_JAVA/Aula002/src/Caneta.java
@@ -0,0 +1,5 @@
+public class Caneta {
+    public static void main(String[] args) {
+        
+    }
+}
